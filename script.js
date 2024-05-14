22362ba9dc0441419e46f40960c843c0903eaef4
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2024-05-13 22:27:46 -0300

    criando function para cada questionario

---
+++ script.js
@@ -0,0 +1,18 @@
+let divCombate = document.getElementById('div-materia-combate');
+let divSalvamento = document.getElementById('div-materia-salvamento');
+let divPrimeiroSocorros = document.getElementById('div-materia-primeiros-socorros');
+
+
+function questoesCombateAIncendio(){
+    alert('foi chamdo a 1 questionario')
+}
+
+
+function questoesSalvamento(){
+    alert('foi chamdo a 2 questionario')
+}
+
+
+function questoesPrimeirosSocorros(){
+    alert('foi chamdo a 3 questionario')
+}
