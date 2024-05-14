a813a5d4771b88f1bb846c4366e9626e8cf90876
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2024-05-14 15:47:44 -0300

    adiciondo location windonw  navegação entre paginas do questionarios

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ let divCombate = document.getElementById('div-materia-combate');
 let divSalvamento = document.getElementById('div-materia-salvamento');
 let divPrimeiroSocorros = document.getElementById('div-materia-primeiros-socorros');
 
+let backPagesQuestions = document.querySelector('.back-pages-questioes');
 
-function questoesCombateAIncendio(){
-    alert('foi chamdo a 1 questionario')
+
+function questoesCombateAIncendio() {
+    window.location.href = "questionarioci.html";
 }
 
-
-function questoesSalvamento(){
-    alert('foi chamdo a 2 questionario')
+function voltarPagesQuestoes() {
+    window.location.href = "index.html";
 }
 
+function questoesSalvamento() {
+    window.location.href = "questionarioSalvamento.html";
+}
 
-function questoesPrimeirosSocorros(){
-    alert('foi chamdo a 3 questionario')
+function questoesPrimeirosSocorros() {
+    window.location.href = "questionarioaph.html";
 }
