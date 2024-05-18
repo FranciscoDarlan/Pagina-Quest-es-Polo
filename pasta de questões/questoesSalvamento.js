ef9ba675baa676749e74550c86c61f56c0404de8
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2024-05-18 15:16:00 -0300

    adicionando questoes em 3 arquivos diferentes. .JS

---
+++ pasta de questões/questoesSalvamento.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        question: "quanto é 1 - 2?",
+        answes: [
+            { option: "3", correct: true },
+            { option: "2", correct: false }, 
+            { option: "4", correct: false },
+        ],
+    },
+    {
+        question: "quanto é 5 - 2?",
+        answes: [
+            { option: "9", correct: false },
+            { option: "10", correct: true }, 
+            { option: "11", correct: false },
+        ],
+    },
+    {
+        question: "quanto é 10 - 8?",
+        answes: [
+            { option: "800", correct: false },
+            { option: "90", correct: false }, 
+            { option: "80", correct: true },
+        ],
+    },
+    {
+        question: "quanto é 7 - 7?",
+        answes: [
+            { option: "70", correct: false },
+            { option: "56", correct: false }, 
+            { option: "49", correct: true },
+        ], 
+    },
+];
